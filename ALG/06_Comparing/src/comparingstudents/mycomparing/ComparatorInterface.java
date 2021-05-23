60ab58e6348184f6f022e79b6a4d63eac25e55f6
fi-GarO <turyji@seznam.cz>
2021-05-23 12:51:11 +0200

    123

--- ALG/06_Comparing/src/comparingstudents/mycomparing/ComparatorInterface.java
+++
@@ -1,9 +0,0 @@
-package comparingstudents.mycomparing;
-
-/**
- *
- * @author janvit
- */
-public interface ComparatorInterface {
-    boolean bigger(Object o1, Object o2);
-}
--- ALG/06_Comparing/src/comparingstudents/mycomparing/CompareInterface.java
+++
@@ -1,9 +0,0 @@
-package comparingstudents.mycomparing;
-
-/**
- *
- * @author janka
- */
-public interface CompareInterface {
-    boolean isBigger(CompareInterface o);
-}
